949adf49976993ecc7cf14bd943996cb9200594b
Steve Reid <steve@appby.design>
2021-01-01 13:17:16 +0000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
-    'plugin:promise/recommended',
     'prettier',
     'prettier/react',
     'prettier/@typescript-eslint',
   ],
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 7,
@@ -18,7 +18,6 @@ module.exports = {
     'import',
     'unused-imports',
     'prettier',
-    'promise',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -34,13 +33,37 @@ module.exports = {
     'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 2,
     'unused-imports/no-unused-vars': 1,
-    'arrow-parens': [
+    'prettier/prettier': 'error',
+    'arrow-body-style': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'react/require-default-props': 0,
+    'import/no-extraneous-dependencies': [
       'error',
-      'as-needed',
       {
-        requireForBlockBody: false,
+        devDependencies: true,
       },
     ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 2,
+    'unused-imports/no-unused-vars': 1,
+    'import/no-anonymous-default-export': 'error',
+    'import/prefer-default-export': 0,
+    'import/no-webpack-loader-syntax': 'off',
+    'react/react-in-jsx-scope': 'off', // React is always in scope with Blitz
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/button-has-type': 0,
+    'jsx-a11y/anchor-is-valid': 'off', // Doesn't play well with Blitz/Next <Link> usage
+    'jsx-a11y/no-noninteractive-tabindex': 0,
   },
   settings: {
     'import/resolver': {
